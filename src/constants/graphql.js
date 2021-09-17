25af6a677da03e2a4be82a7ae557a1bff0423b34
Rouholah <rouholah@gmail.com>
2021-09-17 12:06:26 +0430

    add PWA

---
+++ src/constants/graphql.js
@@ -0,0 +1,30 @@
+import gql from 'graphql-tag'
+
+
+
+export const TEST=gql`
+    query Test{
+        ashkhas {
+            edges {
+                node {
+                    Fname
+                }
+            }
+        }
+    }
+`
+
+export const LOGIN = gql`
+    # 2
+    mutation LoginToSite($username: String!, $password: String!) {
+        login(
+            username: $username,
+            password: $password,
+        ){
+            user{
+                id
+            }
+            token
+        }
+    }
+`
